3db90766a7c9926798956eda356f930957ec22e4
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-02-05 17:31:12 +0900

    feat - 조회용 샘플 데이터 입력

--- src/main/java/com/study/domain/OrderItem.java
+++ src/main/java/com/study/domain/OrderItem.java
@@ -23,4 +23,30 @@ public class OrderItem {
     private int orderPrice; //주문 가격
     private int count; //주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        // 아래를 사로 생성자로 생성안하고 set으로 하는 이유는
+        // 중간에 변동사항이 있기 때문이다.
+        // ex) 상품 할인 등등
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비지니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
