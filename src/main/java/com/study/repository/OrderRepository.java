4550c6f178fd048cdb21c369e37439d4a069509f
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-02-06 16:20:00 +0900

    feat - fetch join 사용

--- src/main/java/com/study/repository/OrderRepository.java
+++ src/main/java/com/study/repository/OrderRepository.java
@@ -94,4 +94,13 @@ public class OrderRepository {
         }
         return query.getResultList();
     }
+
+    //OrderRepository 추가 코드
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .getResultList();
+    }
 }
